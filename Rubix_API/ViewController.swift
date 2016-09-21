babbd1a06ef105cc7bdb0d95d19391f5db6bb210
John Vincent Riccione <jvriccione@Johns-MBP.lfytina2.metronetinc.com>
2016-09-21 00:19:23 -0400

    added camera and basic UI

--- Rubix_API/ViewController.swift
+++ Rubix_API/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import MobileCoreServices
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     let urlImg = "https://c1.staticflickr.com/5/4034/4544827697_6f73866999_b.jpg"
     
@@ -19,13 +20,44 @@ class ViewController: UIViewController {
         findMainColorOfImageWithURL(URL: urlImg)
     }
     
+    @IBOutlet weak var imageView: UIImageView!
+    var newMedia: Bool?
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBAction func useCamera(_ sender: AnyObject) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+            
+            newMedia = true
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [AnyHashable: Any]!) {
+        imageView.image = image
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     func findMainColorOfImageWithURL(URL: String) {
         let headers: HTTPHeaders = [
             "X-Mashape-Key": "7y2as4tWCBmshfUT7x8ymCW1h37ip1fnQ7jjsnMpCwqgvOsstZ",
             "Accept": "application/json"
         ]
         
-        Alamofire.request("https://apicloud-colortag.p.mashape.com/tag-url.json?palette=simple&sort=relevance&url=https://c1.staticflickr.com/5/4034/4544827697_6f73866999_b.jpg", headers: headers).responseJSON { response in
+        Alamofire.request("https://apicloud-colortag.p.mashape.com/tag-url.json?palette=simple&sort=relevance&url=\(URL)", headers: headers).responseJSON { response in
             
             print(response.debugDescription)
             
